853f22e544f9e96dea925a6b086fa5e16e107792
Logan Heitkamp <148575311+Logan-Heitkamp@users.noreply.github.com>
2025-03-02 03:40:19 -0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'dart:io';
+
+bool horizontal = true;
 
 void main() {
   runApp(DraggableNumberApp());
@@ -10,6 +14,7 @@ class DraggableNumberApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: DraggableNumberScreen(),
+      theme: ThemeData.dark(),
       debugShowCheckedModeBanner: false,
     );
   }
@@ -20,15 +25,20 @@ class DraggableNumberScreen extends StatefulWidget {
   _DraggableNumberScreenState createState() => _DraggableNumberScreenState();
 }
 
-
-
 List<Offset> setupPositions(List<String> problem) {
-    List<Offset> positions = [];
-    
-    for (int i = 0; i < problem.length; i ++) {
-      positions.add(Offset((200 + 100 * i).toDouble(), 500));
-    }
-    return positions;
+  List<Offset> positions = [];
+  int currentX = 50;
+
+  for (int i = 0; i < problem.length; i++) {
+    positions.add(Offset(currentX.toDouble(), 500));
+    currentX += problem[i].length * 25;
+  }
+  print(currentX);
+  double offset = ((1825 - currentX + 50) / 2);
+  for (int i = 0; i < positions.length; i++) {
+    positions[i] += Offset(offset, 0);
+  }
+  return positions;
 }
 
 List<bool> setupCorrect(List<String> problem) {
@@ -43,11 +53,10 @@ List<bool> setupCorrect(List<String> problem) {
 List<String> setupInputs(List<String> problem) {
   List<String> inputs = [];
   for (var _ in problem) {
-    inputs.add("Drop Here");
+    inputs.add(" ");
   }
 
   return inputs;
-
 }
 
 List<dynamic> setupMain() {
@@ -64,19 +73,21 @@ List<dynamic> setupMain() {
   List<String> inputs = setupInputs(problem);
   List<String> answers = List<String>.from(options);
 
-  print([question, positions, originalPositions, correct, options, numBoxes, inputs, answers].length);
-  print([question, positions, originalPositions, correct, options, numBoxes, inputs, answers]);
-
-  return [question, positions, originalPositions, correct, options, numBoxes, inputs, answers];
+  return [
+    question,
+    positions,
+    originalPositions,
+    correct,
+    options,
+    numBoxes,
+    inputs,
+    answers,
+  ];
 }
 
-
 class _DraggableNumberScreenState extends State<DraggableNumberScreen> {
-
   // get file contents
   List<dynamic> values = setupMain();
-
-  
 
   late String question = values[0];
   late List<Offset> positions = values[1];
@@ -86,6 +97,7 @@ class _DraggableNumberScreenState extends State<DraggableNumberScreen> {
   late int numBoxes = values[5];
   late List<dynamic> inputs = values[6];
   late List<String> answers = values[7];
+  bool first = true;
 
   void place(int index) {
     options[index] = "";
@@ -100,90 +112,108 @@ class _DraggableNumberScreenState extends State<DraggableNumberScreen> {
     for (int i = 0; i < numBoxes; i++) {
       if (inputs[i] == answers[i]) {
         correct[i] = true;
-      }
-      else {
+      } else {
         replaceOption(i, answers[i]);
       }
     }
-    print(answers);
-    print(options);
-    print(inputs);
-    print(correct);
-    // for (var entry in incorrect_numbers.asMap().entries) {
-    //   int index = entry.key;
-    //   String item = entry.value;
-    //   replaceOption(index, item);
-    // }
-    
+
+    if (first) {
+      first = false;
+    }
+
+
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
+      backgroundColor: const Color.fromARGB(255, 56, 56, 56),
       appBar: AppBar(title: Text("Drag the Correct Answer")),
       body: Stack(
         children: [
-
           // Create question
-          Positioned(
-            left: 100,
-            top: 50,
-            child: Text(
-              question,
-              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+          Align(
+            alignment: Alignment.topCenter,
+            child: Container(
+              padding: const EdgeInsets.all(25),
+              child: Text(
+                question,
+                style: TextStyle(
+                  fontSize: 48,
+                  fontWeight: FontWeight.bold,
+                  decoration: TextDecoration.underline,
+                ),
+              ),
             ),
           ),
 
           ...List.generate(numBoxes, (index) {
-          return Positioned(
-            left: 50 + 210 * index.toDouble(),
-            top: 150,
-            child: DragTarget<String>(
-              onAccept: (value) {
-                setState(() {
-                  if (correct[index]) {
-                    replaceOption(options.indexOf(value), value);
-                  }
-                  else {
-                    inputs[index] = value;
-                    for (var option in options) {
-                      if (value == option) {
-                        place(answers.indexOf(option));
+            return Positioned(
+              left: horizontal ? ((1825 - (answers.length * 200 + (answers.length - 1) * 10)) / 2) + 210 * index.toDouble() : 510,
+              top: horizontal ? 200 : 70 * index + 150,
+              child: Container(
+                padding: const EdgeInsets.only(left: 50, top: 10),
+                child: DragTarget<String>(
+                  onAccept: (value) {
+                    setState(() {
+                      if (correct[index]) {
+                        replaceOption(options.indexOf(value), value);
+                      } else {
+                        inputs[index] = value;
+                        for (var option in options) {
+                          if (value == option) {
+                            place(answers.indexOf(option));
+                          }
+                        }
                       }
-                    }
-                  }
-                });
-              },
-              builder: (context, candidateData, rejectedData) {
-                return Container(
-                  width: 200,
-                  height: 50,
-                  alignment: Alignment.center,
-                  decoration: BoxDecoration(
-                    border: Border.all(color: Colors.black),
-                    color: correct[index] ? Colors.green : Colors.red,
-                  ),
-                  child: Text(
-                    correct[index] ? answers[index] : inputs[index].toString(),
-                    style: TextStyle(fontSize: double.tryParse(inputs[index]) != null ? 36 : 24, fontWeight: FontWeight.bold, height: 1),
-                  ),
-                );
-              },
-            ),
-          );
+                    });
+                  },
+                  builder: (context, candidateData, rejectedData) {
+                    return Container(
+                      width: horizontal ? 200 : 800,
+                      height: horizontal ? 60 : 60,
+                      alignment:
+                          horizontal ? Alignment.center : Alignment.centerLeft,
+                      decoration: BoxDecoration(
+                        border: Border.all(color: first || correct[index] ? Colors.black : Color.fromARGB(255, 255, 110, 100), width: 3),
+                        color:
+                            correct[index]
+                                ? Colors.green : Color.fromRGBO(0, 23, 63, 1),
+                      ),
+                      child: Text(
+                        correct[index]
+                            ? answers[index]
+                            : inputs[index].toString(),
+                        style: TextStyle(
+                          fontSize:
+                              double.tryParse(inputs[index]) != null ? 36 : 24,
+                          fontWeight: FontWeight.bold,
+                          height: 1,
+                        ),
+                      ),
+                    );
+                  },
+                ),
+              ),
+            );
           }),
 
-          Positioned(
-            left: 500,
-            top: 500,
-            child: ElevatedButton(
-              onPressed: () {
-                setState(() {
-                  submitButton();
-                });
-              },
-            child: Text('BUTTON', style: TextStyle(fontSize: 100),),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Container(
+              padding: const EdgeInsets.only(bottom: 100),
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: const Color.fromRGBO(0, 23, 63, 1),
+                  foregroundColor: Colors.white,
+                ),
+                onPressed: () {
+                  setState(() {
+                    submitButton();
+                  });
+                },
+                child: Text('Submit', style: TextStyle(fontSize: 80)),
+              ),
             ),
           ),
 
@@ -195,13 +225,16 @@ class _DraggableNumberScreenState extends State<DraggableNumberScreen> {
                 number: options[index],
                 onDragEnd: (details) {
                   setState(() {
-                    positions[index] = details.offset + Offset(0, -50);
+                    positions[index] =
+                        options[index] != ""
+                            ? details.offset + Offset(0, -50)
+                            : Offset(10000, 1000000);
                   });
                 },
               ),
             );
           }),
-
+          Align(alignment: Alignment.center),
         ],
       ),
     );
@@ -219,17 +252,41 @@ class DraggableNumber extends StatelessWidget {
     return Draggable<String>(
       data: number,
       feedback: Material(
-        color: Colors.transparent,
-        child: Text(
-          number.toString(),
-          style: TextStyle(fontSize: 48, fontWeight: FontWeight.bold, color: Colors.blue),
+        child: Container(
+          decoration: BoxDecoration(
+            border: Border.all(
+              color: const Color.fromRGBO(255, 255, 255, 1),
+              width: 3,
+            ),
+            borderRadius: BorderRadius.all(Radius.circular(5)),
+            color: const Color.fromARGB(255, 75, 75, 75),
+          ),
+          child: Text(
+            number.toString(),
+            style: TextStyle(
+              fontSize: 48,
+              fontWeight: FontWeight.bold,
+              color: const Color.fromRGBO(255, 255, 255, 1),
+            ),
+          ),
         ),
       ),
       childWhenDragging: Container(),
       onDragEnd: onDragEnd,
-      child: Text(
-        number.toString(),
-        style: TextStyle(fontSize: 48, fontWeight: FontWeight.bold, color: Colors.black),
+      child: Container(
+        decoration: BoxDecoration(
+          border: Border.all(color: const Color.fromRGBO(0, 0, 0, 1), width: 3),
+          borderRadius: BorderRadius.all(Radius.circular(5)),
+          color: const Color.fromARGB(255, 75, 75, 75),
+        ),
+        child: Text(
+          number.toString(),
+          style: TextStyle(
+            fontSize: 48,
+            fontWeight: FontWeight.bold,
+            color: const Color.fromRGBO(255, 255, 255, 1),
+          ),
+        ),
       ),
     );
   }
